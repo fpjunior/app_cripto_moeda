c0aece7b2db1f84052a4029e89c067acb126c216
fernando pedro <fernandopedro@MacBook-Pro-de-fernando.local>
2021-12-01 23:25:52 -0300

    installing and configuring provider and shared_preferences

---
+++ lib/configs/app_settings.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppSettings extends ChangeNotifier {
+  late SharedPreferences _prefs;
+  Map<String, String> locale = {
+    'local': 'pt_BR',
+    'name': 'R\$',
+  };
+
+  AppSettings() {
+    _startSettings();
+  }
+
+  _startSettings() async {
+    await _startPreferences();
+    await _readLocale();
+  }
+
+  Future<void> _startPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
+
+  _readLocale() async {
+    final locale = _prefs.getString('local') ?? 'pt_BR';
+    final name = _prefs.getString('name') ?? 'R\$';
+    this.locale = {'local': locale, 'name': name};
+
+    notifyListeners();
+  }
+
+  Future<void> setLocale(String local, String name) async {
+    await _prefs.setString('local', local);
+    await _prefs.setString('name', name);
+    await _readLocale();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aula1/configs/app_settings.dart';
 import 'package:flutter_aula1/meu_aplicativo.dart';
+import 'package:flutter_aula1/repositories/favoritas_repository.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MeuAplicativo());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AppSettings(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => FavoritasRepository(),
+        ),
+      ],
+      child: MeuAplicativo(),
+    ),
+  );
 }
